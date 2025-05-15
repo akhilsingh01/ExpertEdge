dd8169aae5d6011d9ead29f753978d107435b253
Akhil Singh <akhilsingh8191@gmail.com>
2025-05-15 22:40:28 +0530

    update

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def Expert_Edge():
     return render_template('home.html')
 
+@app.route("/courses")
+def courses():
+    return render_template('courses.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
